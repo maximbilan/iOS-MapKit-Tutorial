7fe50bcaf5cac359e7017ef498316d2004886575
Maxim Bilan <maximb.mail@gmail.com>
2016-01-12 20:41:21 +0200

    Added current location button

--- MapKitTutorial/ViewController.swift
+++ MapKitTutorial/ViewController.swift
@@ -15,13 +15,34 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
 	
 	@IBOutlet weak var mapView: MKMapView!
 	
+	// MARK: - Search
+	
+	private var searchController: UISearchController!
+	private var localSearchRequest: MKLocalSearchRequest!
+	private var localSearch: MKLocalSearch!
+	private var localSearchResponse: MKLocalSearchResponse!
+	
+	// MARK: - Map variables
+	
 	private var annotation: MKAnnotation!
 	private var locationManager: CLLocationManager!
+	private var isCurrentLocation: Bool = false
+	
+	// MARK: - Activity Indicator
+	
+	private var activityIndicator: UIActivityIndicatorView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
+		
+		let currentLocationButton = UIBarButtonItem(title: "Current Location", style: UIBarButtonItemStyle.Plain, target: self, action: "currentLocationButtonAction:")
+		self.navigationItem.leftBarButtonItem = currentLocationButton
+		
 		mapView.delegate = self
+	}
+	
+	func currentLocationButtonAction(barButton: UIBarButtonItem) {
+		
 	}
 
 }
